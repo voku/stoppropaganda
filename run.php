75a2d999e1627b17755d31aa4b7a8ddd008dd383
lmoelleken <lmoelleken@meerx-it.com>
2022-02-28 09:22:08 +0100

    support old php versions

--- run.php
+++ run.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use voku\helper\Bootup;
 use voku\stoppropaganda\StopPropaganda;
 use function Amp\ParallelFunctions\parallelMap;
 use function Amp\Promise\wait;
@@ -10,14 +11,19 @@ $urls = include __DIR__ . '/src/voku/stoppropaganda/data/url_targets.php';
 
 shuffle($urls);
 
-wait(
-    parallelMap($urls, static function ($url) {
-        try {
-            $stopPropaganda = new StopPropaganda([$url]);
-            $stopPropaganda->start();
-        } catch (\Throwable $e) {
-            // DEBUG
-            //var_dump($e->__toString());
-        }
-    })
-);
+if (Bootup::is_php('7.4')) {
+    wait(
+        parallelMap($urls, static function ($url) {
+            try {
+                $stopPropaganda = new StopPropaganda([$url]);
+                $stopPropaganda->start();
+            } catch (\Throwable $e) {
+                // DEBUG
+                //var_dump($e->__toString());
+            }
+        })
+    );
+} else {
+    $stopPropaganda = new StopPropaganda($urls);
+    $stopPropaganda->start();
+}
